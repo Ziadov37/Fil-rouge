0bbdad1ec974c7744fecbc2c52a794f11eee1177
Ziadov37 <77000963+Ziadov37@users.noreply.github.com>
2021-06-18 10:15:43 +0100

    UML

--- app/view/pages/Login.php
+++ app/view/pages/Login.php
@@ -7,8 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <!--  CSS  -->
 
@@ -28,35 +27,30 @@
                     <img src="<?php echo URLROOT; ?>/img/login.jpg" class="img-fluid" alt="">
                 </div>
                 <div class="col-lg-7 px-5 pt-5">
-                    <h1 class="font-weight-bold py-5 pl-3">YouCode</h1>
-                    <form action="<?php echo URLROOT; ?>/AdminController/login" method="POST" onsubmit="validation()"
-                        name="form" id="form">
+                    <h1 class="font-weight-bold py-5 pl-3">Annonce Maroc</h1>
+                    <form action="<?php echo URLROOT; ?>/AdminController/login" method="POST" onsubmit="validation()" name="form" id="form">
                         <div class="from-row">
                             <div class="col-lg-7">
                                 <?php if (isset($data['email_err'])) {
                                     echo $data['email_err'];
                                 } ?>
-                                <input type="email" placeholder="Email" name="email" id="email"
-                                    class="form-control email my-3 p-4 <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>">
-                                <div class="alert alert-danger custom-alert"
-                                    style="padding: 5px 10px;margin-bottom: 5px;display: none;">
+                                <input type="email" placeholder="Email" name="email" id="email" class="form-control email my-3 p-4 <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>">
+                                <div class="alert alert-danger custom-alert" style="padding: 5px 10px;margin-bottom: 5px;display: none;">
                                     The username can't be Empty
                                 </div>
                             </div>
                         </div>
                         <div class=" from-row">
                             <div class="col-lg-7">
-                                <input type="password" placeholder="password" name="password" id="password"
-                                    class=" password form-control my-3 p-4 <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>">
-                                <div class="alert alert-danger custom-alert"
-                                    style="padding: 5px 10px;margin-bottom: 5px;display: none;">
+                                <input type="password" placeholder="password" name="password" id="password" class=" password form-control my-3 p-4 <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>">
+                                <div class="alert alert-danger custom-alert" style="padding: 5px 10px;margin-bottom: 5px;display: none;">
                                     The password can't be Empty
                                 </div>
                             </div>
                         </div>
                         <div class=" from-row">
                             <div class="col-lg-7">
-                                <button type="submit" class="btn1 mt-3 mb-5" name="submit">Log in</button>
+                                <button type="submit" class="btn1-secondary mt-3 mb-5" name="submit">Log in</button>
                             </div>
                         </div>
                     </form>
@@ -88,11 +82,9 @@
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 
     <script src="<?php echo URLROOT; ?>/js/jquery-3.4.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
     </script>
     <script src="<?php echo URLROOT; ?>/js/validation.js"></script>
 </body>
